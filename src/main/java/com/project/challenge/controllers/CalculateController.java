a5acbc8d771eccf664dd79ffe3bced5fbd346db6
Diego Iturra <diturra@dparadig.com>
2025-10-12 13:29:25 -0300

    refactor: delete unnecesary import statements

--- src/main/java/com/project/challenge/controllers/CalculateController.java
+++ src/main/java/com/project/challenge/controllers/CalculateController.java
@@ -1,7 +1,6 @@
 package com.project.challenge.controllers;
 
 import com.project.challenge.services.CalculateService;
-import com.project.challenge.services.ExternalPercentageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
