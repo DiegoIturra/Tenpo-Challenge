69753f5f6bc0eac4002b06626ec4f465c8acbbcc
Diego Iturra <diturra@dparadig.com>
2025-10-12 18:28:31 -0300

    refactor: add a key to the percentage value in cache

---
+++ src/main/java/com/project/challenge/config/CacheConfig.java
@@ -0,0 +1,28 @@
+package com.project.challenge.config;
+
+import java.time.Duration;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(30))
+                .maximumSize(100)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("percentageCache");
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}
