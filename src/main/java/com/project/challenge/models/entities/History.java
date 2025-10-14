369aed8e5398d3031cb897eafa9c25ef98b00c15
Diego Iturra <diturra@dparadig.com>
2025-10-14 20:58:01 -0300

    fix: change updatable to true in date field in History class

--- src/main/java/com/project/challenge/models/entities/History.java
+++ src/main/java/com/project/challenge/models/entities/History.java
@@ -7,25 +7,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "history")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@NoArgsConstructor
 public class History {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, updatable = false)
+    @Column(nullable = false)
     private LocalDateTime date;
 
     @Column(nullable = false)
