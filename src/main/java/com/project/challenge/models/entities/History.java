751db22a60f50f9bb402ed4a9a8813d6ed0e1950
Diego Iturra <diturra@dparadig.com>
2025-10-14 00:47:28 -0300

    feat: create first entity for history table using JPA

--- src/main/java/com/project/challenge/models/entities/History.java
+++ src/main/java/com/project/challenge/models/entities/History.java
@@ -1,2 +1,43 @@
-package com.project.challenge.models.entities;public class History {
+package com.project.challenge.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "History")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    private String endpoint;
+
+    @Column(columnDefinition = "TEXT")
+    private String parameters;
+
+    @Column(columnDefinition = "TEXT")
+    private String response;
+
+    @Column(columnDefinition = "TEXT")
+    private String error;
+
 }
