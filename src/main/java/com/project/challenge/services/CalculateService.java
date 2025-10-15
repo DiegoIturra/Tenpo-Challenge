eb29a6ed82aa7d546dc4713a4720952955658081
Diego Iturra <diturra@dparadig.com>
2025-10-14 23:41:41 -0300

    refactor: change method of calculate into small method to divide responsabilities

--- src/main/java/com/project/challenge/services/CalculateService.java
+++ src/main/java/com/project/challenge/services/CalculateService.java
@@ -1,5 +1,6 @@
 package com.project.challenge.services;
 
+import com.project.challenge.models.entities.History;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,24 @@ public class CalculateService {
     ExternalPercentageService percentageService;
 
     @Autowired
+    HistoryAsyncService historyAsyncService;
+
+    @Autowired
     CacheManager cacheManager;
+
+    public Double calculate(int num1, int num2) {
+        String endpoint = buildEndpoint(num1, num2);
+        String parameters = buildParameters(num1, num2);
+
+        try {
+            Double result = doCalculate(num1, num2);
+            saveSuccessRecord(endpoint, parameters, result);
+            return result;
+        } catch (Exception exception) {
+            saveErrorRecord(endpoint, parameters, exception);
+            throw exception;
+        }
+    }
 
     private Double fetchPercentage(){
         try {
@@ -29,9 +47,38 @@ public class CalculateService {
         }
     }
 
-    public Double calculate(int num1, int num2) {
+    private String buildEndpoint(int num1, int num2) {
+        return "/calculate?num1=".concat(String.valueOf(num1))
+                .concat("&num2=")
+                .concat(String.valueOf(num2));
+    }
+
+    private String buildParameters(int num1, int num2) {
+        return "{ num1: " + num1 + ", num2: " + num2 + " }";
+    }
+
+    private Double doCalculate(int num1, int num2) {
         Double percentage = fetchPercentage();
         return (num1 + num2) + percentage;
     }
 
+    private void saveSuccessRecord(String endpoint, String parameters, Double result) {
+        History record = new History();
+        record.setEndpoint(endpoint);
+        record.setError(null);
+        record.setParameters(parameters);
+        record.setResponse("{ result: " + result + " }");
+        historyAsyncService.saveHistoryAsync(record);
+        System.out.println("SAVING A RECORD WITHOUT ERROR: " + record);
+    }
+
+    private void saveErrorRecord(String endpoint, String parameters, Exception exception){
+        History record = new History();
+        record.setEndpoint(endpoint);
+        record.setError(exception.getMessage());
+        record.setParameters(parameters);
+        record.setResponse(null);
+        historyAsyncService.saveHistoryAsync(record);
+        System.out.println("SAVING A RECORD WITH AN ERROR: " + exception.getMessage());
+    }
 }
