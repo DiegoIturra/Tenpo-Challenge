12996e162952c3035c8624cd99b07bb0b518c140
Diego Iturra <diturra@dparadig.com>
2025-10-14 23:40:17 -0300

    feat: create another service to handle async operation of save a record

--- src/main/java/com/project/challenge/services/HistoryAsyncService.java
+++ src/main/java/com/project/challenge/services/HistoryAsyncService.java
@@ -1,2 +1,22 @@
-package com.project.challenge.services;public class HistoryAsyncService {
+package com.project.challenge.services;
+
+import com.project.challenge.models.entities.History;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HistoryAsyncService {
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Async
+    public void saveHistoryAsync(History record) {
+        try {
+            historyService.save(record);
+        } catch (Exception ex) {
+            System.out.println("Error saving record: " + ex.getMessage());
+        }
+    }
 }
