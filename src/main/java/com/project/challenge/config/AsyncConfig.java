7f835afc5a2fed3f44fd3686c2d26ccfc5272fde
Diego Iturra <diturra@dparadig.com>
2025-10-14 23:26:54 -0300

    feat: change inheritance from CrudRepository to JpaRepository to use Pagination

---
+++ src/main/java/com/project/challenge/config/AsyncConfig.java
@@ -0,0 +1,2 @@
+package com.project.challenge.config;public class AsyncConfig {
+}
--- src/main/java/com/project/challenge/repositories/HistoryRepository.java
+++ src/main/java/com/project/challenge/repositories/HistoryRepository.java
@@ -1,10 +1,6 @@
 package com.project.challenge.repositories;
 
 import com.project.challenge.models.entities.History;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HistoryRepository extends CrudRepository<History, Long> {
-
-
-
-}
+public interface HistoryRepository extends JpaRepository<History, Long> { }
