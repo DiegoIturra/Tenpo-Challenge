5db38c18aa3fc8c7bc42921d42d8d484f022f7fa
Diego Iturra <diturra@dparadig.com>
2025-10-15 23:32:21 -0300

    refactor: modify method getPercentage simulating with a probability of 50% a failure and the other 50% a success request

--- src/main/java/com/project/challenge/services/ExternalPercentageServiceImpl.java
+++ src/main/java/com/project/challenge/services/ExternalPercentageServiceImpl.java
@@ -1,13 +1,28 @@
 package com.project.challenge.services;
 
 import com.project.challenge.services.interfaces.ExternalPercentageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ExternalPercentageServiceImpl implements ExternalPercentageService {
 
+    private static final Logger log = LoggerFactory.getLogger(ExternalPercentageServiceImpl.class);
+
     @Override
     public Double getPercentage(){
+
+        double failureChance = Math.random();
+
+        if(failureChance < 0.5) {
+            log.error("External service failure");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "External service failure");
+        }
+
+        log.info("External service success");
         Double randomNumber = Math.random() * 100;
         return Math.round(randomNumber * 100.0) / 100.0;
     }
